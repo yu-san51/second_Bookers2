66ad5915a481b05939e32c9e6229d862d698ce47
yu-san51 <nekonyan.yuki51@gmail.com>
2020-04-02 02:29:10 +0000

    [Add] validation,flash

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+
+  validates :name, presence: true, length: { minimum: 2, maxmum: 50 }
+  validates :introduction, length: { minimum: 2, maxmum: 50 }
+
   has_many :books, dependent: :destroy
   attachment :profile_image
 end
